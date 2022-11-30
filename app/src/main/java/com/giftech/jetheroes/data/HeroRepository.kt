00e1fc8d92ad992195c6135cb8e606c96b9dc693
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-11-30 13:01:39 +0700

    add searchHeroes func

--- app/src/main/java/com/giftech/jetheroes/data/HeroRepository.kt
+++ app/src/main/java/com/giftech/jetheroes/data/HeroRepository.kt
@@ -7,4 +7,9 @@ class HeroRepository {
     fun getHeroes(): List<Hero> {
         return HeroesData.heroes
     }
+    fun searchHeroes(query: String): List<Hero>{
+        return HeroesData.heroes.filter {
+            it.name.contains(query, ignoreCase = true)
+        }
+    }
 }
